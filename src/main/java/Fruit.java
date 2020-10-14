8b99636c55723074c8a4f0c23047763221217871
Sarah Usher <sarahnusher@gmail.com>
2020-10-14 14:19:01 +0100

    Remove other files to start from a clean slate

--- src/main/java/Fruit.java
+++
@@ -1,4 +0,0 @@
-public interface Fruit {
-    int getPrice();
-    int getNumber();
-}
---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void checkTest(){
+        assertEquals(1, 2);
+    }
+}
