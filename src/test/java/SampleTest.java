9166687f9ec51f9a204d8d6715ed7463549189cd
Sarah Usher <sarahnusher@gmail.com>
2020-10-14 14:20:42 +0100

    Purposefully breaking test to see that test mechanism works

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,10 +2,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Test {
+public class SampleTest {
 
     @org.junit.jupiter.api.Test
-    public void checkTest(){
+    public void checkFailingTest(){
         assertEquals(1, 2);
     }
 }
